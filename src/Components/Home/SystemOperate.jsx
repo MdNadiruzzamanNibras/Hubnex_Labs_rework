c3a33fc8563f4eb0aa3a1407c9ca3b9cba2aff7a
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2024-03-19 20:03:48 +0600

    system operate

--- src/Components/Home/SystemOperate.jsx
+++ src/Components/Home/SystemOperate.jsx
@@ -9,32 +9,30 @@ const SystemOperate = () => {
         <span className="font-medium">Operates</span>
       </h1>
       <div className="flex justify-center items-center">
-        <div className="w-96 pl-4">
-          <div className="flex justify-between">
-            {" "}
-            <h2 className="mt-10 text-xl font-bold">Upload Documents</h2>
-            <div>
-              <img src={upload} alt="" />
-            </div>
-          </div>
-
-          <p className="w-80">
+        <div className="w-96 h-64 pl-4 relative">
+          {" "}
+          <h2 className="mt-10 text-xl font-bold">Upload Documents</h2>
+          <img className="absolute ml-[270px] mt-[-68px]" src={upload} alt="" />
+          <p className="w-80 mt-10">
             Shortlist the most qualified candidate and upload their details for
             the top companies
           </p>
         </div>
-        <div className="w-96 bg-purple-300 m-0 pl-4 h-80">
+        <div className="w-96 bg-purple-300 m-0 pl-4 h-64  relative">
           <h2 className="mt-10 text-xl font-bold">Sign Up</h2>
 
           <p className="w-80 mt-10">
             Follow the link below to sign up and get access of the current job
             postings
           </p>
-          <div className="flex justify-end items-end">
-            <img src={signUp} alt="" />
-          </div>
+
+          <img
+            className=" absolute ml-[270px] mb-[-200px]"
+            src={signUp}
+            alt=""
+          />
         </div>
-        <div className="w-96 pl-4">
+        <div className="w-96 h-64 pl-4 ">
           <div className="flex justify-between">
             {" "}
             <h2 className="mt-10 text-xl font-bold">Get Rewards</h2>
@@ -43,7 +41,9 @@ const SystemOperate = () => {
             </div>
           </div>
 
-          <p>As soon as the candidate gets selected you get your benefits</p>
+          <p className="w-80">
+            As soon as the candidate gets selected you get your benefits
+          </p>
         </div>
       </div>
     </div>
